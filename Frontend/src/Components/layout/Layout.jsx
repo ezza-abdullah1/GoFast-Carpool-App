f65f419f978305201b69b28a8c3e715569a84bac
Ahmad Shamail Butt <134318985+AhmadShamailButt@users.noreply.github.com>
2025-04-15 00:58:24 +0500

    Revert "Update Layout.jsx"
    This reverts commit 349d3647eea56eefcfbc85d824da1f8cd43cecdc.

--- Frontend/src/Components/layout/Layout.jsx
+++ Frontend/src/Components/layout/Layout.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
 function Layout() {
   return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
+    <div><Header/>
+        <Outlet/>
+    <Footer/></div>
+  )
 }
 
-export default Layout;
+export default Layout
