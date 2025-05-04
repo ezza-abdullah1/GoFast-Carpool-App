1127b8e24692cdd72b7d027678e8a70deec1117d
Tehreem Tahir <tehreem.4861@gmail.com>
2025-05-04 23:31:40 +0500

    dependencies

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,13 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
-    process.exit(1);
+    const conn = await mongoose.connect(
+      "mongodb+srv://GoFastCarpoolApp:1234567890@cluster0.gwox1e8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1); // Exit the process with failure
   }
 };
 
