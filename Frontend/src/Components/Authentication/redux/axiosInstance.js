290289c8eb1fa32fb3a43b7d627b4a79b1dab214
Ahmad Shamail Butt <134318985+AhmadShamailButt@users.noreply.github.com>
2025-05-09 01:54:20 +0500

    removed workflows

--- Frontend/src/Components/Authentication/redux/axiosInstance.js
+++ Frontend/src/Components/Authentication/redux/axiosInstance.js
@@ -10,7 +10,7 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = sessionStorage.getItem("token");  // Get token from session storage
+    const token = sessionStorage.getItem("token");  
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
