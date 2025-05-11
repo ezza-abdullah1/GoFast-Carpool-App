58d5a56921a95e89c2419d617c7e6cb00a3ab0b3
Tehreem Tahir <tehreem.4861@gmail.com>
2025-05-11 23:04:11 +0500

    updated User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -38,6 +38,14 @@ const userSchema = new mongoose.Schema({
      resetCode: String,  
 resetPasswordToken: String,
 resetPasswordExpires: Date,
+isOnline: {
+      type: Boolean,
+      default: false,
+    },
+    lastSeen: {
+      type: Date,
+      default: Date.now,
+    },
 
 
 });
